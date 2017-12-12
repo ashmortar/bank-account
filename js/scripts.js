b5895defec576330dbc75e4fdf566e3527017210
Aaron Ross and Jun Li <jun@>
2017-12-12 14:29:56 -0800

    working version with all functionality

--- js/scripts.js
+++ js/scripts.js
@@ -16,25 +16,60 @@ Account.prototype.withdraw = function(withdrawal) {
   return this.balance;
 }
 
-
-
+var accounts = [];
+var accounter = 0;
 
 //front end logic ----------------------
 
 $(document).ready(function() {
+  var resetFields = function() {
+    $("#register-name").val("");
+    $("#register-passWord").val("");
+    $("#initial-deposit").val("");
+    $("#alter-name").val("");
+    $("#alter-passWord").val("");
+    $("#deposit").val("");
+    $("#withdrawal").val("");
+  }
 
   //register an account actions
   $("form#register").submit(function(event) {
     event.preventDefault();
     var name = $("#register-name").val();
-    var password = $("#resgist-passWord").val();
-    var balance = parseInt($("#initial-deposit").val());
+    var password = $("#register-passWord").val();
+    var balance = parseFloat($("#initial-deposit").val());
+    console.log(name);
+    accounts.push(new Account(name, password, balance));
 
+    $("#current-balance").append('<div class="well"><h4>Name: ' + accounts[accounter].name + '</h4><br>' + '<h4>Balance: $<span  id="account' + accounter + '">' + accounts[accounter].balance + '</span>');
+    accounter++;
+    resetFields();
   })
 
   //alter ad account actions
   $("form#alter").submit(function(event) {
     event.preventDefault();
+
+    var name = $("#alter-name").val();
+    var password = $("#alter-passWord").val();
+    var deposit = parseFloat($("#deposit").val());
+    var withdrawal = parseFloat($("#withdrawal").val());
+    console.log(name);
+    console.log(password);
+    console.log(deposit);
+    console.log(withdrawal);
+    for (var i = 0; i < accounts.length; i ++) {
+    console.log(accounts.length);
+      if((name === accounts[i].name) && (password === accounts[i].password)) {
+        accounts[i].deposit(deposit);
+        accounts[i].withdraw(withdrawal);
+        var updateBalance = eval("'#account" + i + "'");
+        $(updateBalance).text(accounts[i].balance);
+      } else {
+        alert("Wrong usename/password combination");
+      }
+    }
+    resetFields();
   })
 
 })
